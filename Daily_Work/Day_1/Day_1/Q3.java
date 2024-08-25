e751440fdd40966b4a00b5aace6cfd880623daf4
Yugandhar Deshmukh <102009488+Yugandhar194@users.noreply.github.com>
2024-08-25 23:04:02 +0530

    Add files via upload

---
+++ Daily_Work/Day_1/Day_1/Q3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Q3 {	//Multiples of 7 
+	public static void main (String args[]) {
+		int i;
+		for(i=7; i<=100;i++) {
+			if(i%7==0) {
+					System.out.println(i);
+			}
+	}
+}
+}
