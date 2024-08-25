e93d5ee2b8ffe5904d09981b7c3a8aa76767ec25
Yugandhar Deshmukh <102009488+Yugandhar194@users.noreply.github.com>
2024-08-25 23:01:55 +0530

    Add files via upload

---
+++ Daily_Work/Day_1/Day_1/Day_2/Demo.java
@@ -0,0 +1,12 @@
+class Demo {
+	public static void main(String args[] ){
+		char c = 'A';
+		System.out.println(c);
+		System.out.println((int)(c));
+		System.out.println((char)(66));
+		System.out.println((char)(500));
+		System.out.println((char)(125));
+		System.out.println((char)(499));
+	
+	}
+}
---
+++ Daily_Work/Day_1/Day_1/Day_2/assignment_2.java
@@ -0,0 +1,5 @@
+public class Main { 
+    public static int main(String[] args) { 
+        System.out.println("Hello, World!"); 
+        return 0; 
+    } 
