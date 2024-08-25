967dc07a244a6f681e98da01a3638511a1d0f841
Yugandhar Deshmukh <102009488+Yugandhar194@users.noreply.github.com>
2024-08-25 22:51:12 +0530

    Add files via upload

---
+++ Daily_Work/Day1/Demo.java
@@ -0,0 +1,10 @@
+class Test{}
+
+class Demo {
+    public static void main(String[] args) {
+        {
+            System.out.println("Hello, Welcome to CDAC");
+            System.out.println(10+10);
+        }
+    }
+}
