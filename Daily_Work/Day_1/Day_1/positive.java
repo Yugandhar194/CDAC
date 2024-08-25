48fa94fbb63bc49bcd0da49fd0e96643b49f578f
Yugandhar Deshmukh <102009488+Yugandhar194@users.noreply.github.com>
2024-08-25 23:00:33 +0530

    Add files via upload

---
+++ Daily_Work/Day_1/Day_1/positive.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class positive {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int number;
+       number = sc.nextInt();
+       if ( number > 0 ) {
+        System.out.println("The number is positive");
+       }
+       else {
+        System.out.println("The number is ngative or zero");
+       }
+    } 
+}
