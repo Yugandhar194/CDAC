bf0db0b4bf97946c84a605c9cd0272d5449a906f
Yugandhar Deshmukh <102009488+Yugandhar194@users.noreply.github.com>
2024-08-31 00:01:26 +0530

    Add files via upload

---
+++ Logic Build Classes/Daily-Work/Day-4/Java.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Java {
+	public static void main (String args[]) {
+		for (int i=0; i<5; i++) {
+			if(true) {
+				System.out.println("xyz");
+				break;
+				
+			}
+			//System.out.println("ABC");
+		}
+		System.out.println("xyz");
+	}
+}                                
