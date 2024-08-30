09c5af47a50043a92dfbd7ca1af0fb41ab44c803
Yugandhar Deshmukh <102009488+Yugandhar194@users.noreply.github.com>
2024-08-30 23:53:46 +0530

    Add files via upload

---
+++ Logic Build Classes/Daily-Work/Day-2/MissingBreakCase.java
@@ -0,0 +1,15 @@
+public class MissingBreakCase { 
+    public static void main(String[] args) { 
+        int level = 1; 
+        switch(level) { 
+            case 1: 
+                System.out.println("Level 1"); 
+            case 2: 
+                System.out.println("Level 2"); 
+            case 3: 
+                System.out.println("Level 3"); 
+            default: 
+                System.out.println("Unknown level"); 
+        } 
+    } 
+}
