7c56eec3c83e7d979ec93cfc4fddd13777b8374b
Yugandhar Deshmukh <102009488+Yugandhar194@users.noreply.github.com>
2024-10-04 19:36:39 +0530

    Update Infix.java

--- Module_3 (ADS)/Assignments/Assignment - 3/InfixToPostfix.java
+++ Module_3 (ADS)/Assignments/Assignment - 3/InfixToPostfix.java
@@ -1,5 +1,6 @@
-import java.util.Stack;
-public class InfixToPostfix {
+public class Infix {
+
+   
     public static int precedence(char ch) {
         switch (ch) {
             case '+':
@@ -14,28 +15,78 @@ public class InfixToPostfix {
         return -1;
     }
 
+    
+    public static class CharStack {
+        char[] stack;
+        int top;
+        int capacity;
+
+        public CharStack(int size) {
+            stack = new char[size];
+            capacity = size;
+            top = -1;
+        }
+
+        public void push(char ch) {
+            if (top == capacity - 1) {
+                System.out.println("Stack overflow");
+            } else {
+                stack[++top] = ch;
+            }
+        }
+
+        public char pop() {
+            if (top == -1) {
+                System.out.println("Stack underflow");
+                return 0;
+            } else {
+                return stack[top--];
+            }
+        }
+
+        public char peek() {
+            if (top == -1) {
+                return 0;
+            } else {
+                return stack[top];
+            }
+        }
+
+        public boolean isEmpty() {
+            return top == -1;
+        }
+    }
+
     public static String convertToPostfix(String expression) {
-        Stack<Character> stack = new Stack<>();
+        int n = expression.length();
+        CharStack stack = new CharStack(n);
         StringBuilder result = new StringBuilder();
 
-        for (char c : expression.toCharArray()) {
+        for (int i = 0; i < n; i++) {
+            char c = expression.charAt(i);
+
             if (Character.isLetterOrDigit(c)) {
                 result.append(c);
-            } else if (c == '(') {
+            }
+            
+            else if (c == '(') {
                 stack.push(c);
-            } else if (c == ')') {
+            }
+    
+            else if (c == ')') {
                 while (!stack.isEmpty() && stack.peek() != '(') {
                     result.append(stack.pop());
                 }
-                stack.pop();
-            } else {
+                stack.pop(); 
+            }
+        
+            else {
                 while (!stack.isEmpty() && precedence(c) <= precedence(stack.peek())) {
                     result.append(stack.pop());
                 }
                 stack.push(c);
             }
         }
-
         while (!stack.isEmpty()) {
             result.append(stack.pop());
         }
@@ -45,8 +96,6 @@ public class InfixToPostfix {
 
     public static void main(String[] args) {
         System.out.println(convertToPostfix("A+B*C"));        
-        System.out.println(convertToPostfix("A*B+C/D"));   
+        System.out.println(convertToPostfix("A*B+C/D"));    
     }
 }
-//Time Complexity: O(n)
-//Space Complexity: O(n)
